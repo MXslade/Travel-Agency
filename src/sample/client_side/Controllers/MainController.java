cf0e9bbb0b7fd0e9baf14a25de7154a4c8a6eb86
Turar <turar0408@gmail.com>
2019-11-03 20:54:58 +0600

    some new additions

--- src/sample/client_side/Controllers/MainController.java
+++ src/sample/client_side/Controllers/MainController.java
@@ -2,4 +2,10 @@ package sample.client_side.Controllers;
 
 public class MainController {
 
+    private static final String ONE_WAY_TICKET = "ONE_WAY_TICKET";
+    private static final String BACK_AND_FORTH_TICKET = "BACK_AND_FORTH_TICKET";
+    private static final String SEVERAL_TICKETS = "SEVERAL_TICKETS";
+
+    
+
 }
